9886098b726249304f2bdfe576f297f6f082df91
Kyle Grealis <kylegrealis@icloud.com>
2024-10-18 12:32:48 -0500

    updating imports

--- R/yUM-package.R
+++ R/yUM-package.R
@@ -1,8 +1,10 @@
 #' @keywords internal
+#' @noRd
 "_PACKAGE"
 
 ## usethis namespace: start
 #' @importFrom here here
 #' @importFrom stringr str_to_lower
+#' @importFrom utils download.file
 ## usethis namespace: end
 NULL
