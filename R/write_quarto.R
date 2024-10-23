6b2bd63b4a5b84ac32d8544827a216f35697f910
Kyle Grealis <kylegrealis@icloud.com>
2024-10-23 16:06:04 -0500

    edit: adding custom & defaults to yaml

--- R/write_quarto.R
+++ R/write_quarto.R
@@ -1,23 +1,19 @@
 #' Start a Quarto file with a formatted template header
 #' 
-#' This function creates a new Quarto document with a pre-formatted YAML header. The user
-#' needs to update the Title, author details (including optional URL link, email address,
-#' and ORCID). An abstract is included along with keywords to 'tag' the document.
+#' This function creates a new Quarto document with a pre-formatted YAML header. The 
+#' default template requires a _variables.yml file. If none exists in the project, 
+#' a prompt is provided to create the file. The _variables.yml allows for a reusable
+#' custom header that will be applied to all \code{froggeR} default-style Quarto 
+#' documents. 
 #' 
-#' Default options have been set for the table of contents and code output. Either set
-#' the options to 'false' (do not quote the options in the YAML) or comment out the 
-#' selected line(s). The default editor is source and not visual.
-#' 
-#' NOTE: The default theme for this is sandstone. Please visit
-#' \url{https://quarto.org/docs/output-formats/html-themes.html} to explore more
-#' theming options.
+#' NOTE: Please visit \url{https://quarto.org/docs/output-formats/html-themes.html} to 
+#' explore more Quarto theming options.
 #' 
 #' @param filename The name of the file. Do not include '.qmd'
 #' @param path The path to the main project level. Defaults to returned value
 #' from \code{here::here()}.
-#' @param default You are the default user. 😉 You NEED to change this, unless you
-#' want a personalized Quarto document with the author's details. \code{*THIS WILL BE
-#' FIXED in a future version!*}
+#' @param default The default is set to TRUE and will create a Quarto template file
+#' that pulls infomation from the folder/_variables.yml file.
 #' @return A Quarto document with formatted YAML and two blank starter sections.
 #' 
 #' @export
@@ -27,23 +23,23 @@
 #' write_quarto(filename = 'new_doc', path = '../path_to_location')
 #' }
 
-write_quarto <- function(filename = 'new', path = here::here(), default = FALSE) {
+write_quarto <- function(filename = 'new', path = here::here(), default = TRUE) {
 
   the_quarto_file <- paste0(path, '/', filename, '.qmd')
   abort <- FALSE
 
-  # path to default Quarto header template
+  # path to default Quarto header template using _variables.yml
   gist_path_default <- paste0(
-    'https://gist.githubusercontent.com/kyleGrealis/693e5d0df41576247900c3bef788d475/',
+    'https://gist.githubusercontent.com/kyleGrealis/60487135cff714dcb8312b8312df9fc7/',
     # this changes as the default Quarto is updated
-    'raw/fe770eab51c8e600f9c353a53f9a078673343b2c/quarto_header.qmd'
+    'raw/fac38bc0c3c63a65f8e252aa562db0177115259e/frogger_quarto.qmd'
   )
 
-  # path to personalized Quarto header template
-  gist_path_kyle <- paste0(
-    'https://gist.githubusercontent.com/kyleGrealis/d8e61fc834f92868823e71ff26bda745/',
+  # path to Quarto header template the user must complete
+  gist_path_other <- paste0(
+    'https://gist.githubusercontent.com/kyleGrealis/693e5d0df41576247900c3bef788d475/',
     # this changes as the personalized Quarto is updated
-    'raw/9f2be2469dd2372617e7bf79166c3417591777df/kyle_quarto_header.qmd'
+    'raw/63b42f341882f24f2c319c1feb74a66f99a30438/quarto_header.qmd'
   )
 
   # Warn user if Quarto document already found in the project
@@ -53,8 +49,20 @@ write_quarto <- function(filename = 'new', path = here::here(), default = FALSE)
   }
 
   if (!abort) {
-    # override gist path for Kyle
-    if (!default) { gist_path_default <- gist_path_kyle }
+    if (default) {
+      
+      # check if _variables.yml exists and prompt user to create if FALSE
+      if (!file.exists(paste0(path, '/_variables.qmd'))) {
+        message('You need to create a custom _variables.yml file. Here we go!')
+        # use the same path as where the Quarto doc will be created
+        froggeR::write_variables(path)
+      }
+      
+    } else {
+      # override gist path for template that doesn't use custom yml file
+      gist_path_default <- gist_path_other 
+    }
+
 
     download.file(gist_path_default, the_quarto_file, quiet = TRUE)
     ui_done('\nA new Quarto file has been created.\n\n')
