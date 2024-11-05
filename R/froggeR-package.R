ffdaaab300653274e124e22059005b63a5bd6d83
Kyle Grealis <kylegrealis@icloud.com>
2024-11-05 10:13:46 -0600

    adding rstudioapi for opening new session

--- R/froggeR-package.R
+++ R/froggeR-package.R
@@ -6,6 +6,7 @@
 #' @importFrom cli col_blue col_green
 #' @importFrom glue glue
 #' @importFrom quarto quarto_create_project
+#' @importFrom rstudioapi openProject
 #' @importFrom stringr str_to_lower
 #' @importFrom usethis ui_done ui_info ui_nope ui_oops ui_todo ui_yeah
 #' @importFrom utils download.file
