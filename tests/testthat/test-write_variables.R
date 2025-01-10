07877346b67ce873cb15074717462c3ab60f72c4
Kyle Grealis <kyleGrealis@icloud.com>
2025-01-10 00:22:08 -0600

    package unit tests; all pass

--- tests/testthat/test-write_variables.R
+++ tests/testthat/test-write_variables.R
@@ -1,5 +1,7 @@
 test_that("write_variables fails with bad path", {
-  expect_error(write_variables(path = 'bad_path'))
+  expect_error(write_variables(path = 'bad_path'), 'Invalid `path`.')
+  expect_error(write_variables(path = ' '), 'Invalid `path`.')
+  expect_error(write_variables(path = NULL), 'Invalid `path`.')
 })
 
 test_that("write_variables creates _variables.yml correctly", {
@@ -27,6 +29,19 @@ test_that("write_variables creates content correctly", {
   expect_true(any(grepl('^orcid: ', file_content)))
   expect_true(any(grepl('^url: ', file_content)))
   expect_true(any(grepl(': Table of Contents$', file_content)))
+  # Clean up
+  unlink(variables_file)
+  expect_false(file.exists(variables_file))
+})
+
+test_that('write_variables recognizes overwrite error', {
+  tmp_dir <- tempdir()
+  variables_file <- file.path(tmp_dir, "_variables.yml")
+  # Remove any prior instances
+  unlink(variables_file)
+  suppressMessages(write_variables(tmp_dir, .initialize_proj = TRUE))
+  # Test for error
+  expect_error(write_variables(tmp_dir, .initialize_proj = TRUE), 'already exists')
   # Clean up
   unlink(variables_file)
   expect_false(file.exists(variables_file))
