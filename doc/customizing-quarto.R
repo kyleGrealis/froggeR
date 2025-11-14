1185e31dbcf2df6bb913580d0765b0e3d58ffb84
Kyle Grealis <kyle@kyleGrealis.com>
2025-11-13 21:38:11 -0600

    feat: Release v0.6.0 -- files for new & updated functions

---
+++ doc/customizing-quarto.R
@@ -0,0 +1,12 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----echo=FALSE, fig.align='center', fig.cap='Link color styling - Before', out.width='70%'----
+knitr::include_graphics("../man/figures/link_color_before.png")
+
+## ----echo=FALSE, fig.align='center', fig.cap='Link color styling - After', out.width='70%'----
+knitr::include_graphics("../man/figures/link_color_after.png")
+
---
+++ doc/hopping-into-quarto.R
@@ -0,0 +1,10 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE---------------------------------------------------------------
+# froggeR::write_readme()
+# froggeR::write_notes()
+
